b95684190c5b5646cfafb16cb2eb2f891c0c5f9f
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-25 01:51:23 -0300

    feat(pedidos): criar PessoaView e PessoaViewModel com salvar, excluir e listar pedidos

--- WpfApp/Views/PedidoWindow.xaml.cs
+++ WpfApp/Views/PedidoWindow.xaml.cs
@@ -1,27 +1,106 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using WpfApp.Models;
+using WpfApp.Services;
 
-namespace WpfApp.Views
+namespace WpfApp.ViewModels
 {
-    /// <summary>
-    /// Lógica interna para PedidoWindow.xaml
-    /// </summary>
-    public partial class PedidoWindow : Window
+    public class PedidoViewModel : INotifyPropertyChanged
     {
-        public PedidoWindow()
+        private readonly PedidoService pedidoService;
+        private readonly PessoaService pessoaService;
+        private readonly ProdutoService produtoService;
+
+        public PedidoViewModel()
         {
-            InitializeComponent();
+            pedidoService = new PedidoService();
+            pessoaService = new PessoaService();
+            produtoService = new ProdutoService();
+
+            Pessoas = new ObservableCollection<Pessoa>(pessoaService.GetAll());
+            Produtos = new ObservableCollection<Produto>(produtoService.GetAll());
+            ItensPedido = new ObservableCollection<ItemPedido>();
+
+            AdicionarProdutoCommand = new RelayCommand(AdicionarProduto);
+            FinalizarPedidoCommand = new RelayCommand(FinalizarPedido);
+        }
+
+        private Pessoa pessoaSelecionada;
+        public Pessoa PessoaSelecionada
+        {
+            get => pessoaSelecionada;
+            set { pessoaSelecionada = value; OnPropertyChanged(nameof(PessoaSelecionada)); }
+        }
+
+        private Produto produtoSelecionado;
+        public Produto ProdutoSelecionado
+        {
+            get => produtoSelecionado;
+            set { produtoSelecionado = value; OnPropertyChanged(nameof(ProdutoSelecionado)); }
+        }
+
+        private int quantidade = 1;
+        public int Quantidade
+        {
+            get => quantidade;
+            set { quantidade = value; OnPropertyChanged(nameof(Quantidade)); }
+        }
+
+        public ObservableCollection<Pessoa> Pessoas { get; set; }
+        public ObservableCollection<Produto> Produtos { get; set; }
+        public ObservableCollection<ItemPedido> ItensPedido { get; set; }
+
+        public decimal ValorTotal => ItensPedido.Sum(i => i.Subtotal);
+
+        // Comandos
+        public ICommand AdicionarProdutoCommand { get; set; }
+        public ICommand FinalizarPedidoCommand { get; set; }
+
+        private void AdicionarProduto()
+        {
+            if (ProdutoSelecionado != null && Quantidade > 0)
+            {
+                var item = new ItemPedido
+                {
+                    Produto = ProdutoSelecionado,
+                    Quantidade = Quantidade
+                };
+                ItensPedido.Add(item);
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        private void FinalizarPedido()
+        {
+            if (PessoaSelecionada == null || !ItensPedido.Any()) return;
+
+            var pedido = new Pedido
+            {
+                Pessoa = PessoaSelecionada,
+                Produtos = ItensPedido.ToList(),
+                Status = Status.Pendente,
+                DataVenda = DateTime.Now,
+                FormaPagamento = "Dinheiro" 
+            };
+
+            pedidoService.AddPedido(pedido);
+
+            // Limpar tela
+            ItensPedido.Clear();
+            PessoaSelecionada = null;
+            ProdutoSelecionado = null;
+            Quantidade = 1;
+            OnPropertyChanged(nameof(ValorTotal));
+        }
+
+        // INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
