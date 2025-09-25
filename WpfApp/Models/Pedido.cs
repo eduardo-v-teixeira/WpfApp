7b47fca03df0c6247d4952fe5c95ec297d56ea67
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-25 00:28:30 -0300

    feat(wpf):Criando estrutura padrão
    Criando as estrutura Pedido e Pessoa

--- WpfApp/Models/Pedido.cs
+++ WpfApp/Models/Pedido.cs
@@ -10,15 +10,15 @@ namespace WpfApp.Models
         public List<ItemPedido> Produtos { get; set; } = new List<ItemPedido>();
         public decimal ValorTotal { get; set; }
         public DateTime DataVenda { get; set; }
-        public string FormaPagamento { get; set; }
-        public string Status { get; set; }
+        public string FormaPagamento { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pendente";
         public bool Finalizado { get; set; } = false;
     }
 
     public class ItemPedido
     {
         public Guid ProdutoId { get; set; }
-        public string NomeProduto { get; set; }
+        public string NomeProduto { get; set; } = string.Empty;
         public decimal ValorUnitario { get; set; }
         public int Quantidade { get; set; }
         public decimal ValorTotal => ValorUnitario * Quantidade;
