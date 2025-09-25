2f0c57c5418770653ff2cc2e7977de7764b805b2
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-25 00:52:18 -0300

    feat(wpf): Implementando pedido e diretório

--- WpfApp/Services/PedidoService.cs
+++ WpfApp/Services/PedidoService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WpfApp.Models;
 
 namespace WpfApp.Services
 {
-    class PedidoService
+    public class PedidoService
     {
+        private readonly string FilePath;
+        private List<Pedido> pedidos;
+
+
+        public PedidoService() // construtor é execultado quando a classe é instanciada
+        {
+            var assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation) ?? "";
+            var projectDirectory = Path.GetFullPath(Path.Combine(assemblyDirectory, "..", "..", ".."));
+            var dataDirectory = Path.Combine(projectDirectory, "Data");
+            FilePath = Path.Combine(dataDirectory, "Pedido.json");
+
+            if (!Directory.Exists(dataDirectory))
+            {
+                Directory.CreateDirectory(dataDirectory);
+            }
+
+            if (File.Exists(FilePath))
+            {
+                var json = File.ReadAllText(FilePath);
+                pedidos = JsonConvert.DeserializeObject<List<Pedido>>(json) ?? new List<Pedido>();
+            }
+            else
+            {
+                pedidos = new List<Pedido>();
+                SaveChanges();
+            }
+        }
     }
-}
